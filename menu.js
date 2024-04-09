4f5c75fb329aa726285d893feca19eb5aa230733
David Arias <edam210@gmail.com>
2024-04-09 17:07:57 -0600

    Boton Claro Oscuro 100%

--- menu.js
+++ menu.js
@@ -1,18 +1,24 @@
+//Funcion Abrir y Cerrar menu
 const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
 
 abrir.addEventListener("click", () => {
-    nav.classList.add("visible");
+  nav.classList.add("visible");
 });
 
 cerrar.addEventListener("click", () => {
-    nav.classList.remove("visible");
+  nav.classList.remove("visible");
 });
 
-const btnSwicth = document.querySelector('#switch');
+function seleccionar(){
+    nav.classList.remove("visible");
+}
 
-btnSwicth.addEventListener('click', () => {
-    document.body.classList.toggle('dark');
-    btnSwicth.classList.toggle('active');
+//Funcion btn Claro Oscuro
+const btnSwicth = document.querySelector("#switch");
+
+btnSwicth.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  btnSwicth.classList.toggle("active");
 });
